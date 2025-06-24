4ec229a845272ce9e39943c39f91fb8d6387a75a
Ahmed Yousry <ahmed29yousry@gmail.com>
2025-06-24 21:58:38 +0300

    app run

---
+++ dist-electron/util.js
@@ -0,0 +1,13 @@
+import { getUIPath } from "./pathResolver.js";
+import { pathToFileURL } from "url";
+export function isDev() {
+    return process.env.NODE_ENV === "development";
+}
+export function validateEventFrame(frame) {
+    if (isDev() && new URL(frame.url).host === "localhost:5123") {
+        return;
+    }
+    if (frame.url !== pathToFileURL(getUIPath()).toString()) {
+        throw new Error("Malicious event");
+    }
+}
